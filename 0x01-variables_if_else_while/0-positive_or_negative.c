4d164df8c565b03529ee53ece4ee1e25174bab69
DanielAbabu <101533329+DanielAbabu@users.noreply.github.com>
2022-04-08 21:02:02 +0300

    Task 0 and readme

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints a random nuber and states whether 
- * 	   it is positive, negative, or zero.
+ *main - Prints a random nuber and states whether
+ *	it is positive, negative, or zero.
  *
  * Return: Always 0
  */
